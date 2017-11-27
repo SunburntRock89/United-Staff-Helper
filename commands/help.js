72a67c108ee65b468f465ac0fd25e6182f3dd2e2
StupidDan <danielbee912@gmail.com>
2017-11-27 18:07:39 +0000

    Added help command

---
+++ commands/help.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message, args) {
+    const embed1 = new Discord.RichEmbed();
+    const embed2 = new Discord.RichEmbed();
+    embed1.setTitle("Unites Staff Helper Help Menu")
+    .setDescription("These are all the commands in the Unites Staff Helper Discord Bot, enjoy! :wink:\n The prefix for Unites Staff Helper is `?`")
+    .addField("about", "Posts an embed with a little bit of info about the bot. `?about`")
+    .addField("ban", "Allows you to ban a specified user, with a reason. `?ban [user] [reason]`")
+    .addField("help", "DMs you this help menu. `?help`")
+    .addField("kick", "Allows you to kick a specified user, with a reason. `?kick [user] [reason]`")
+    .addField("ping", "Pings the connection of the bot with a milliseconds value. `?ping`")
+    .addField("warn", "Allows you to warn a user, with a reason. `?warn [user] [reason]`")
+    .setColor("#add8e6")
+    .setThumbnail("https://cdn.discordapp.com/attachments/384749294042742785/384753378468823050/Unites_Staff_Helper_Avatar.png")
+    .setFooter("Be sure to DM Dan.#2518 or any other developer of Unites Staff Helper if you bump into any problems or glitches with the bot.")
+    message.author.send({embed: embed1});
+
+    embed2.setTitle("Help!!")
+    .setDescription("Check your DMs. :mailbox:")
+    .setColor("add8e6")
+    message.channel.send({embed: embed2});
+}
