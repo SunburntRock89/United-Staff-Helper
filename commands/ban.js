423b3fabbf6972bb0c0de121f3bc1586fdefad5e
StupidDan <danielbee912@gmail.com>
2017-11-27 17:55:57 +0000

    Added main moderation commands
    Ban, kick, and warn.

---
+++ commands/ban.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message, args) {
+    const embed1 = new Discord.RichEmbed();
+    const embed2 = new Discord.RichEmbed();
+    let member = message.mentions.members.first();
+    let reason = message.content.split(/\s+/g).slice(2).join(" ");
+    if (message.mentions.users.size == 0) {
+        return message.channel.send(`**${message.author} You need to mention a user! :angry:**`);
+    }
+    if (message.mentions.users.size > 1) {
+        return message.channel.send(`**${message.author} You can only mention one user! :anguished:**`);
+    }
+    if (reason.length == 0) {
+        return message.channel.send(`**${message.author} You need to include a reason! :face_palm:**`);
+    }
+
+    embed1.setTitle(`${member.user.tag} has been banned!`)
+    .setDescription(`${member.user.tag} has been banned! :hammer:`)
+    .setColor("#98fb98")
+    message.channel.send({embed: embed1});
+
+    embed2.setTitle("You've been banned!")
+    .setDescription(`It looks like you've been banned by ${message.author} on ${message.guild.name}! :hammer:`)
+    .setColor("#ff0000")
+    message.mentions.members.first().send({embed: embed2});
+
+    member.ban(reason);
+}
