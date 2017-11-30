c6f5796d3dd32016d0a20a884039385e6c150eff
Dking1009 <34109521+Dking1009@users.noreply.github.com>
2017-11-30 17:47:45 -0500

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,16 +1,41 @@
-exports.run = function(client, message, args) {
-    function getRandom(min, max) {
-        ran = Math.floor(Math.random() * (max - min + 1)) + min;
-        return ran;
-      }
-    let question = getRandom(0, 1, 2);
-    if (question == "0") {
-        message.channel.send(`${message.author} Yes. :smiley:`)
-    }
-    if (question == "1") {
-        message.channel.send(`${message.author} No. :frowning:`)
-    }
-    if (question == "2") {
-        message.channel.send(`${message.author} Maybe. :smirk:`)
-    }
-}
+// Gives a random 8ball response
+
+const responses = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good!',
+    'Yes~!',
+    'Signs point to yes.',
+    'Reply hazy try again...',
+    'Ask again later.',
+    'Better not tell you now...',
+    'Cannot predict now...',
+    'Concentrate and ask again.',
+    'Don\'t count on it.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good...',
+    'Very doubtful.'
+  ]
+  
+  exports.run = (client, message) => {
+    message.reply(responses[Math.floor(Math.random() * responses.length)])
+  }
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  }
+  
+  exports.help = {
+    name: '8ball',
+    description: 'Random 8ball response to questions',
+    usage: '8ball'
+  }
