800f7311a3bf91d5d40384cf99daf5c832bf9b42
Dking1009 <34109521+Dking1009@users.noreply.github.com>
2017-12-01 15:56:00 -0500

    Updated

--- commands/Logger.js
+++ commands/Logger.js
@@ -1,31 +1,75 @@
-const winston = require('winston');
+import { getLastResult } from '../databases/auditlogs'
+import { getMinutes, getHours } from '../engine/timeutils'
 
-const logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)({
-        timestamp: function() {
-          return new Date().toISOString() 
-        },
-        formatter: function(options) {
-        // Return string will be passed to logger.
-        return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
-          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+const Config = require('../config.json')
+const Dash = require('rethinkdbdash')
+let r = new Dash({
+  user: Config.database.user,
+  password: Config.database.pass,
+  silent: true,
+  servers: [{
+    host: Config.database.host,
+    port: Config.database.port
+  }]
+})
+
+function getChannel (guildID, bot) {
+  return new Promise(function (resolve, reject) {
+    r.db('Guilds').table('all').filter({
+      'guildID': guildID
+    }).run().then((lc) => {
+      if (lc[0].logchannel === '' || lc[0].logchannel === undefined || !lc) {
+        // Ignore no logchannel responses
+      } else {
+        let logChannel = bot.Channels.get(lc[0].logchannel)
+        resolve(logChannel)
+      }
+    }).catch(e => {
+      reject(e)
+    })
+  })
+}
+
+function channelCreated (c, bot) {
+  getChannel(c.channel.guild_id, bot).then((lc) => {
+    getLastResult(bot, c.channel.guild_id).then((res) => {
+      if (res === false) {
+        // Ignore
+      } else {
+        lc.sendMessage(`:new: [\`${getHours()}:${getMinutes()}\`] User \`${res.perpetrator.username}#${res.perpetrator.discriminator}\` created a ${res.type} channel: **${res.channelName}** (${res.channelID})`)
+      }
+    }).catch(_ => {
+      // No audit log access or something else; ignore
+    })
+  })
+}
+
+function channelUpdated (c, bot) {
+  getChannel(c.channel.guild_id, bot).then((lc) => {
+    getLastResult(bot, c.channel.guild_id).then((res) => {
+      if (res === false) {
+        // Do nothing
+      } else {
+        if (res.before === res.after) {} else {
+          lc.sendMessage(`:gear: [\`${getHours()}:${getMinutes()}\`] User \`${res.perpetrator.username}#${res.perpetrator.discriminator}\` edited a ${res.type} channel's name: Before: **${res.before}** | After: **${res.after}**`)
         }
-     }),
-      new (winston.transports.File)({ 
-        filename: 'logfile.log',
-        timestamp: function() {
-          return new Date().toISOString() 
-        },
-        formatter: function(options) {
-        // Return string will be passed to logger.
-        return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
-          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
-        }
-     })
-    ]
-  });
+      }
+    })
+  })
+}
 
-logger.emitErrs = false;
+function channelDeleted (c, bot) {
+  getChannel(c.data.guild_id, bot).then((lc) => {
+    getLastResult(bot, c.data.guild_id).then((res) => {
+      if (res === false) {
+        // Do nothing
+      } else {
+        lc.sendMessage(`🚮 [\`${getHours()}:${getMinutes()}\`] User \`${res.perpetrator.username}#${res.perpetrator.discriminator}\` deleted a ${res.type} channel: **${res.channelName}** (${res.channelID})`)
+      }
+    }).catch(_ => {
+      // Ignore
+    })
+  })
+}
 
-module.exports.logger = logger;
+export { channelCreated, channelUpdated, channelDeleted, getChannel }
