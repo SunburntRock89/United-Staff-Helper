d51044d015a2b56645b118a2e5121150d0d5a4db
StupidDan <danielbee912@gmail.com>
2017-12-08 21:43:48 +0000

    Added suggestion command and added it to the help menu

---
+++ commands/suggestion.js
@@ -0,0 +1,4 @@
+exports.run = function(client, message, args) {
+    message.channel.send(`**${message.author} Check your DMs. :mailbox_with_no_mail:**`);
+    message.author.send("**Feel free to suggestion stuff for the bot using this form:** https://goo.gl/qccASD");
+}
